babed34a07d6f8bf16ad92811d908afdad1ae854
lsnerson <97560009+lsnerson@users.noreply.github.com>
2023-03-28 16:16:57 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,3 +1,4 @@
+// create all necessary variables
 let video = document.querySelector("#player1");
 let play_button = document.querySelector("#play");
 let pause_button = document.querySelector("#pause");
@@ -10,6 +11,7 @@ let volume = document.querySelector("#volume");
 let styled = document.querySelector("#vintage");
 let original = document.querySelector("#orig");
 
+//load function to load the page, turn off autoplay and loop
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.load();
@@ -19,27 +21,32 @@ window.addEventListener("load", function() {
 	console.log("loop: " + video.loop)
 });
 
+//when play is clicked, put volume at 100% text and play the video
 play_button.addEventListener("click", function(){
 	video.play();
 	volume.innerHTML = video.volume * 100 + "%";
 	console.log("Play Video");
 })
 
+//when pause is clicked pause the video
 pause_button.addEventListener("click", function(){
 	video.pause();
 	console.log("Pause Video");
 })
 
+//when slow down is clicked, slow the video by 10% each time its clicked
 slow_down.addEventListener("click", function(){
 	video.playbackRate *= 0.90;
 	console.log(video.playbackRate);
 })
 
+//when speed up is clicked, increase the speed 
 speed_up.addEventListener("click", function(){
 	video.playbackRate *= 1.10;
 	console.log(video.playbackRate);
 })
 
+//when skip ahead is clicked, advance the video by 10 seconds
 skip_ahead.addEventListener("click", function(){
 	if (video.currentTime + 10 >= 67.403333){
 		video.currentTime = 0
@@ -51,6 +58,7 @@ skip_ahead.addEventListener("click", function(){
 	console.log(video.currentTime)
 })
 
+//mute and unmite the video and update the text
 mute.addEventListener("click", function(){
 	if (video.muted == true){
 		mute_button.innerHTML = "Mute";
@@ -64,21 +72,21 @@ mute.addEventListener("click", function(){
 	}
 })
 
+//chnage the volume based on the slider and update the text
 volume_slider.addEventListener("change", function(){
 	video.volume = volume_slider.value / 100;
 	volume.innerHTML = video.volume * 100 + "%";
 })
 
-styled.addEventListener("click", function(){
-	video.style = document.getElementsByClassName(".oldSchool");
-	// console.log("It is styled.");
-})
-
+//change style to old school when button is clicked
 styled.addEventListener("click", function(){
 	video.className += " oldSchool";
+	console.log("It is styled.");
 })
 
+//change style back to original when button is clicked
 original.addEventListener("click", function(){
 	video.className = "video";
+	console.log("It is not styled.");
 })
 
